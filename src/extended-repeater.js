7b351adb75b5212b1028cfd6630ed449d4d48c05
Mary Zhavnerovich <rostova0106@gmail.com>
2020-03-18 11:21:12 +0300

    fit: recursive depth solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,3 @@
 module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+  throw 'Not implemented';  
 };
-  
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,15 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr, count = 1) {
+        if (Array.isArray(arr) === false) {
+            throw new Error('this is not array');
+        } 
+            for(let i = 0; i<arr.length; i++) {
+                if (Array.isArray(arr[i])) {
+                    arr = arr.flat();
+                    count = this.calculateDepth(arr, ++count);
+                    break;
+                }
+            }
+            return count;
     }
 };
