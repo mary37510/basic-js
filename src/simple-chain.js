b7be015656a5ee05257c35ce1a9b20db104a7ba4
Mary Zhavnerovich <rostova0106@gmail.com>
2020-03-21 15:12:25 +0300

    feat: add solution simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,33 @@
 const chainMaker = {
+  arr: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.arr.length;
   },
+  
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.arr.push(value);
+    return this;
   },
+
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position === 'string' || position <= 0 || position > this.arr.length) {
+      this.arr = [];
+      throw new Error();
+    }
+    this.arr.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let copy = [...this.arr];
+    this.arr = [];
+    return copy.map(el => `( ${el} )`).join('~~');
   }
 };
 
